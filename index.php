2ac154479a079ebb7dc759f5d4e24774be8a374f
giovanna <giovannaolivera@gmail.com>
2014-12-29 17:00:41 -0800

    Added top navigation bar to music.html, css changes to index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="icon" href="../../favicon.ico">
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style_main.css">
+    <link rel="stylesheet" href="css/style.css">
   	<script type="text/javascript" src="js/quotes.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>    
 
@@ -20,42 +20,35 @@
 	<div class="site-wrapper">
 			
 		<div class="site-wrapper-inner">
-			<div class="inner" style="background-color: #3333FF;">
-			<nav style="margin-bottom: 40px;">
-			<h3 class="mastheader-brand" style="font-size:35px; font-weight:bold; font-family:sans-serif">Bliss</h3>
-			
-				<ul class="nav mastheader-nav">
-					<li class="active"><a href="#">Home</a></li>
-					<li><a href="music.html">Music</a></li>
-					<li><a href="meditation.html">Meditation</a></li>
-					<li><a href="drawing.html">Drawing</a></li>
-					<li><a href="about.html">About</a></li>
-				</ul>
-             </nav>
-			</div>
-        <div class="site-container">
-
-          
-          <div class="mastheader clearfix">
-          </div>
-
-			<div class="inner cover">
-				<p class="lead" style = "margin-top:150px; padding: 30px; background-color: #E6F0FF; color: #000; font-size: 32px; font-family: cursive; font-style: italic; display: none;">
-				<script>
-					$( '.lead' ).fadeIn( 1500, lines()); //ensure element is set to display: none for this to work!
-				</script>
-				</p>
+			<div class="inner_new">
+				<nav style="margin-bottom: 40px;">
+				<h3 class="mastheader-brand" style="font-size:35px; font-weight:bold; font-family:sans-serif">Bliss</h3>
 				
+					<ul class="nav mastheader-nav">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="music.html">Music</a></li>
+						<li><a href="meditation.html">Meditation</a></li>
+						<li><a href="drawing.html">Drawing</a></li>
+						<li><a href="about.html">About</a></li>
+					</ul>
+	             </nav>
 			</div>
-			
+	        <div class="site-container">
+	          	<div class="mastheader clearfix">
+				  	<div class="cover">
+						<p class="lead">
+						<script>
+							$( '.lead' ).fadeIn( 1500, lines()); //ensure element is set to display: none for this to work!
+						</script>
+						</p>				
+					</div>
+	            </div>
+			</div>
         </div>
-
-      </div>
 	
     </div>
 	<div class="site-footer">
-            
-			</div>
+    </div>
 			
 </body>
 </html>
